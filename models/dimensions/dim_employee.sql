792e5518a55632c8dacf57cef27f73cd0be080b7
Andy Sawyer <andy@nydas.co>
2024-06-10 15:14:51 +1000

    Make dim_employee slowly changing

--- models/dimensions/dim_employee.sql
+++ models/dimensions/dim_employee.sql
@@ -4,56 +4,56 @@
         )
 }}
 
-WITH
-department AS (
-    SELECT
-        id AS department_bk,
-        department AS department_name
-    FROM
-        {{ ref('snp_department')}}
-    WHERE
-        1 = 1
-        AND dbt_valid_to IS NULL
+WITH 
+se AS (
+	SELECT
+		id,
+		department_id,
+		CONCAT(last_name, ', ', first_name) AS employee_name,
+		dbt_valid_from,
+		COALESCE(dbt_valid_to, '2999-12-31') AS dbt_valid_to
+	FROM
+		{{ ref('snp_employee') }}
 ),
-
-employee AS (
-    SELECT
-        MD5(id::VARCHAR) AS employee_hk,
-        id AS employee_bk,
-        CONCAT(last_name, ', ', first_name) AS employee_name,
-        department_id AS department_bk
-    FROM
-        {{ ref('snp_employee')}}
-    WHERE
-        1 = 1
-        AND dbt_valid_to IS NULL
+sd AS (
+	SELECT 
+		id,
+		department,
+		dbt_valid_from,
+		COALESCE(dbt_valid_to, '2999-12-31') AS dbt_valid_to
+	FROM
+		{{ ref('snp_department') }}
 ),
-
 final AS (
-    SELECT
-        employee.employee_hk,
-        employee.employee_bk,
-        employee.employee_name,
-        COALESCE(department.department_name, '') AS department_name
-    FROM
-        employee
-    LEFT JOIN department
-        ON employee.department_bk = department.department_bk
-    UNION
-    SELECT
-        '-1' AS employee_hk,
-        -1 AS employee_bk,
-        'Not Applicable' AS employee_name,
-        'Not Applicable' AS department_name
-    UNION
-    SELECT
-        '-2' AS employee_hk,
-        -2 AS employee_bk,
-        'Unknown' AS employee_name,
-        'Unknown' AS department_name
+	SELECT
+		se.id AS _employee_bk,
+		GREATEST(
+			COALESCE(se.dbt_valid_from, '1900-01-01'),
+			COALESCE(sd.dbt_valid_from, '1900-01-01')
+		) AS _valid_from_datetime,
+		LEAST(
+			COALESCE(se.dbt_valid_to, '2999-12-31'),
+			COALESCE(sd.dbt_valid_to, '2999-12-31')
+		) AS _valid_to_datetime,
+		se.employee_name,
+		sd.department
+	FROM
+		se
+		LEFT JOIN sd
+		  ON se.department_id = sd.id
+		  AND se.dbt_valid_from <= sd.dbt_valid_to
+		  AND se.dbt_valid_to > sd.dbt_valid_from
 )
-
-SELECT
-    *
-FROM    
-    final
+SELECT 
+	MD5(CONCAT(_employee_bk::VARCHAR, _valid_from_datetime::VARCHAR)) AS employee_hk,
+	_employee_bk,
+	_valid_from_datetime AS _load_datetime,
+	CASE 
+		WHEN LAG(_valid_from_datetime) OVER (PARTITION BY _employee_bk ORDER BY _valid_from_datetime) IS NULL THEN '1900-01-01'
+		ELSE _valid_from_datetime
+	END AS _valid_from_datetime,
+	_valid_to_datetime,
+	employee_name,
+	department
+FROM
+	final
