71c8f9c380ade19b52209c78c623f79eda43d5e6
Andy Sawyer <andy@nydas.co>
2024-08-13 11:19:48 +1000

    changes

---
+++ models/dimensions/dim_date.sql
@@ -0,0 +1,51 @@
+{{ 
+    config(
+        enabled=true
+        )
+}}
+
+WITH date_range AS (
+SELECT 
+  UNNEST(RANGE('2024-01-01'::DATE, '2024-12-31'::DATE, '1 day'::interval))::DATE AS date_id
+),
+
+final AS (
+    SELECT
+        CONCAT(datepart('year', date_id)::STRING,
+            lpad(datepart('month', date_id)::STRING, 2, '0'), 
+            lpad(datepart('day', date_id)::STRING, 2, '0'))::INT AS _date_hk,
+        date_id,
+        datepart('day', date_id)::INT AS day_number,
+        strftime(date_id, '%A, %-d %B %Y') AS formatted_date,
+        CONCAT(datepart('year', date_id)::STRING, lpad(datepart('month', date_id)::STRING, 2, '0'))::INT AS _year_month_sort,
+        strftime(date_id, '%B %Y') AS month_year,
+        datepart('year', date_id)::INT AS year_number,
+        datepart('quarter', date_id)::INT AS quarter_number
+    FROM 
+        date_range
+    UNION
+    SELECT
+        19000101 AS _date_hk,
+        '1900-01-01'::DATE AS date_id,
+        -1 AS day_number,
+        'Not Applicable' AS formatted_date,
+        -1 AS _year_month_sort,
+        'Not Applicable' AS month_year,
+        -1 AS year_number,
+        -1 AS quarter_number
+    UNION
+    SELECT
+        19000102 AS _date_hk,
+        '1900-01-02'::DATE AS date_id,
+        -2 AS day_number,
+        'Unknown' AS formatted_date,
+        -2 AS _year_month_sort,
+        'Unknown' AS month_year,
+        -2 AS year_number,
+        -2 AS quarter_number
+)
+
+SELECT
+    *
+FROM
+    final
--- models/facts/fct_sale.sql
+++ models/facts/fct_sale.sql
@@ -31,12 +31,20 @@ d_region AS (
         {{ ref('dim_region') }}
 ),
 
+d_date AS (
+    SELECT
+        _date_hk,
+        date_id
+    FROM
+        {{ ref('dim_date') }}
+),
+
 sale AS (
     SELECT
         COALESCE(d_client._client_hk, '-2') AS _client_hk,
         COALESCE(d_employee._employee_hk, '-2') AS _employee_hk,
         COALESCE(d_region._region_hk, '-2') AS _region_hk,
-        sale.date::DATE AS sale_date,
+        COALESCE(d_date._date_hk, -2) AS _date_hk,
         SUM(sale.sale) AS sale_amount
     FROM
         {{ ref('stg_sale') }} AS sale
@@ -48,6 +56,8 @@ sale AS (
             AND sale.date::DATE < d_employee._valid_to_datetime
         LEFT JOIN d_region
             ON COALESCE(sale.region, 'Unknown') = d_region.region_name
+        LEFT JOIN d_date
+            ON COALESCE(sale.date, 'Unknown') = d_date.date_id
     WHERE
         1 = 1
         AND sale.dbt_valid_to IS NULL
@@ -55,7 +65,7 @@ sale AS (
         _client_hk,
         _employee_hk,
         _region_hk,
-        sale_date
+        _date_hk
 )
 
 SELECT
